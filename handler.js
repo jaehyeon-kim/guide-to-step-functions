9d3034832655e827a861a4ed3beba216c79d44a1
Jaehyeon Kim <jaehyeon.kim@cloudwave.com.au>
2020-12-18 12:24:38 +1100

    up catch

--- handler.js
+++ handler.js
@@ -8,6 +8,14 @@ module.exports.add = async ({ x, y }) => {
   return x + y;
 };
 
+class NumberIsSoBig extends Error {
+  constructor(n) {
+    super(`${n} is so big!`);
+    this.name = 'NumberIsSoBig';
+    Error.captureStackTrace(this, NumberIsSoBig);
+  }
+}
+
 class NumberIsTooBig extends Error {
   constructor(n) {
     super(`${n} is too big!`);
@@ -17,6 +25,18 @@ class NumberIsTooBig extends Error {
 }
 
 module.exports.double = async (n) => {
+  if (n > 40 && n <= 50) {
+    throw new NumberIsSoBig(n);
+  }
+
+  if (n > 50) {
+    throw new NumberIsTooBig(n);
+  }
+
+  return n * 2;
+};
+
+module.exports.doubleBigNumber = async (n) => {
   if (n > 50) {
     throw new NumberIsTooBig(n);
   }
